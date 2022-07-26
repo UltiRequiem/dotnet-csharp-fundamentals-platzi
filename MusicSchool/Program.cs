f6b4dd4ee7c743ed81495d228404b9c26ccf0cef
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-26 16:59:43 -0500

    :sparkles: Update

---
+++ MusicSchool/Program.cs
@@ -0,0 +1,50 @@
+﻿class School
+{
+    public string name;
+    public string direction;
+    public int fundationYear;
+    public string director;
+
+    public School(string schoolName, string schoolDirection, int schoolFundationYear, string schoolDirector)
+    {
+        name = schoolName;
+        direction = schoolDirection;
+        fundationYear = schoolFundationYear;
+        director = schoolDirector;
+
+    }
+
+    public void ring()
+    {
+        Console.Beep(987, 1000);
+        Console.Beep(1174, 500);
+        Console.Beep(880, 1500);
+
+        Console.Beep(783, 250);
+        Console.Beep(880, 250);
+        Console.Beep(987, 1000);
+
+        Console.Beep(1174, 500);
+        Console.Beep(880, 1500);
+
+        Console.Beep(987, 1000);
+        Console.Beep(1174, 500);
+        Console.Beep(1760, 1000);
+        Console.Beep(1567, 500);
+        Console.Beep(1174, 1000);
+
+        Console.Beep(1046, 250);
+        Console.Beep(987, 250);
+        Console.Beep(880, 1000);
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var school = new School("Platzi", "Lima", 2000, "Freddy");
+        school.ring();
+    }
+}
